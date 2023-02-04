5f10d417318f40c3abd12d6f158dc85292dd9c9f
unknown <mateuszkowalczyk7@gmail.com>
2023-02-04 14:33:39 +0100

    fixed rendering button and improved mobile view

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let searchedMovie
 let moviesArr 
 let moviesOnWatchlist = []
 let savedMovies = []
+let isAdded = false
 
 //Render watchlist
 function renderWatchlist(){
@@ -63,9 +64,9 @@ document.body.addEventListener("click", function(e){
 })
 
 
-
 //Search for movies
 searchBtn.addEventListener("click",function(){
+    let idsInStorage = localStorage.getItem("moviesOnWatchlist")
     document.getElementById("movieList").innerHTML = ``
     //I grab movie title to use it in api call
     searchedMovie = searchBox.value
@@ -83,13 +84,38 @@ searchBtn.addEventListener("click",function(){
                 //map is used because multiple (array of) movies may contain same string in name
                 resultsArr.map(function(result){
                     fetch(`http://www.omdbapi.com/?i=${result.imdbID}&apikey=d7841fbf`)
-
+            
                     .then(res => res.json())
                     .then(movieData =>{
                         return movieData
                     })
                     //Render html with results
                     .then(moviesArr =>{
+                        //check if movie is already liked to change its icon
+                        if(idsInStorage.includes(moviesArr.imdbID)){
+                            document.getElementById("movieList").innerHTML += `
+                            <div class="movie">
+                                <div class="mainWrap">
+                                    <img src="${moviesArr.Poster}" alt="" class="poster">
+                                    <div class="infoWrap">
+                                        <div class="titleWrap">
+                                            <h3>${moviesArr.Title}</h3>
+                                            <h4 class="rating"><i class="fa-solid fa-star"></i> ${moviesArr.imdbRating}</h4>
+                                        </div>
+                                        <div class="detailsWrap" id="details">
+                                            <p>${moviesArr.Runtime}</p>
+                                            <p>${moviesArr.Genre}</p>
+                                            <p class="added"><i class="fa-solid fa-circle-check" data-like="true"></i> Already on watchlist</p>
+                                        </div>
+                                        <div class="description">
+                                            <p>${moviesArr.Plot}</p>
+                                        </div>
+                                    </div> 
+                                </div>
+                            `
+                        }
+            
+                        else {
                         document.getElementById("movieList").innerHTML += `
                         <div class="movie">
                             <div class="mainWrap">
@@ -99,10 +125,10 @@ searchBtn.addEventListener("click",function(){
                                         <h3>${moviesArr.Title}</h3>
                                         <h4 class="rating"><i class="fa-solid fa-star"></i> ${moviesArr.imdbRating}</h4>
                                     </div>
-                                    <div class="detailsWrap">
+                                    <div class="detailsWrap" id="details">
                                         <p>${moviesArr.Runtime}</p>
                                         <p>${moviesArr.Genre}</p>
-                                        <p class="addToWatchlist" data-filmid="${moviesArr.imdbID}"><i class="fa-solid fa-circle-plus" data-filmid="${moviesArr.imdbID}"></i> Watchlist</p>
+                                        <p id="${moviesArr.imdbID}" class="addToWatchlist" data-filmid="${moviesArr.imdbID}"><i id="addIcon" class="fa-solid fa-circle-plus" data-filmid="${moviesArr.imdbID}" data-like="false"></i> Watchlist</p>
                                     </div>
                                     <div class="description">
                                         <p>${moviesArr.Plot}</p>
@@ -110,6 +136,7 @@ searchBtn.addEventListener("click",function(){
                                 </div> 
                             </div>
                         `
+                    }
                     })
                 })
                
@@ -132,23 +159,26 @@ function hidePlaceholder() {
 
 //Add to watchlist
 document.body.addEventListener("click", function(e){
-    //this var enables adding IDs if localStorage is empty
+    //this var enables adding IDs if localStorage is not empty
     let moviesAlreadyOnWatchlist = localStorage.getItem("moviesOnWatchlist")
     if(e.target.dataset.filmid && moviesAlreadyOnWatchlist){
         //first I need to get localstorage, otherwise it was replacing old ids with new
         moviesOnWatchlist = JSON.parse(localStorage.getItem("moviesOnWatchlist")) 
-        moviesOnWatchlist.push(e.target.dataset.filmid)   
-        console.log(moviesOnWatchlist)
+        moviesOnWatchlist.push(e.target.dataset.filmid)
         localStorage.setItem("moviesOnWatchlist", JSON.stringify(moviesOnWatchlist))
-    } 
+        console.log(e.target.dataset.filmid)
+        document.getElementById(e.target.dataset.filmid).innerHTML = `<i class="fa-solid fa-circle-check added" data-like="true"></i> Already on watchlist`
+        document.getElementById(e.target.dataset.filmid).classList.add("added")
+        }
     
     else if(e.target.dataset.filmid && moviesAlreadyOnWatchlist === null){
         //If localstorage is empty, theres no need to get old ids first as nothing will be replaced
         moviesOnWatchlist.push(e.target.dataset.filmid)   
         console.log(moviesOnWatchlist)
-        localStorage.setItem("moviesOnWatchlist", JSON.stringify(moviesOnWatchlist)) 
+        localStorage.setItem("moviesOnWatchlist", JSON.stringify(moviesOnWatchlist))
+        document.getElementById(e.target.dataset.filmid).innerHTML = `<i class="fa-solid fa-circle-check added" data-like="true"></i> Already on watchlist`
+        document.getElementById(e.target.dataset.filmid).classList.add("added")    
     }
 
 })
-
 
